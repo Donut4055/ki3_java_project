4ba277fb5015f1f0781f644d841d620044d3e098
Donut4055 <phongdztv112234@gmail.com>
2025-04-17 15:46:35 +0700

    task_3

--- project/src/ra/edu/business/service/account/AccountServiceImpl.java
+++ project/src/ra/edu/business/service/account/AccountServiceImpl.java
@@ -1,20 +1,20 @@
 package ra.edu.business.service.account;
 
 
-import ra.edu.business.dao.account.AccountDAOImpl;
+import ra.edu.business.dao.account.IAccountDAO;
 import ra.edu.business.model.Account;
 
 public class AccountServiceImpl implements IAccountService {
-    private AccountDAOImpl accountDAOImpl;
+    private IAccountDAO accountDAO;
 
-    public AccountServiceImpl(AccountDAOImpl accountDAO) {
-        this.accountDAOImpl = accountDAO;
+    public AccountServiceImpl(IAccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
     }
 
     @Override
     public Account login(String username, String password, String role) {
         try {
-            return accountDAOImpl.login(username, password, role);
+            return accountDAO.login(username, password, role);
         } catch (Exception e) {
             System.out.println("Lỗi đăng nhập: " + e.getMessage());
             return null;
